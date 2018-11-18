790c77282b35690ebee27e4d4dfc2663c0a3616e
Shawn Xu <xx9483@gmail.com>
2018-11-18 14:34:07 -0800

    Environment setup. * A Dockerfile that based on ubuntu, with python 3 and boto 3 installed. * A bash script that mounts necessary folder(.aws) with container.

---
+++ scripts/create_table.py
@@ -0,0 +1,7 @@
+'''
+This script require credentials with dynamo db write access(suggest AmazonDynamoDBFullAccess).
+More about boto3 credentials: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/quickstart.html#configuration
+'''
+import boto3
+
+
---
+++ start_dev_env.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#This script will start the development environment for you.
+set -e
+
+BOT=$1
+
+
+docker run \
+  -it --rm \
+  --name ffly-poem-dev-env  \
+  --mount type=bind,source="$(pwd)",target=/ffly-poem \
+  --mount type=bind,source="$(pwd)/.aws",target=/.aws \
+  ffly-poem-dev-env
