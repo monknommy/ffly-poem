b521df9145cbeef8ba6b00706cb333e992114e50
Shawn Xu <xx9483@gmail.com>
2019-01-09 17:35:13 -0800

    add react router

--- client/poem-web/src/App.js
+++ client/poem-web/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
 import ApolloClient from "apollo-boost";
+import React, { Component } from 'react';
 import gql from "graphql-tag";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import './App.css';
 
@@ -23,10 +24,39 @@ client
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <Router>
+        <div>
+          <Route path="/poem/:id" component={Poem} />
+          <Route path="/author/:id" component={Author} />
+          <Route exact path="/" component={Home} />
+          </div>
+      </Router>
     );
   }
 }
 
+function Home() {
+  return (
+    <div>
+      <h3>Home</h3>
+    </div>
+  );
+}
+
+
+function Poem({ match }) {
+  return (
+    <div>
+      <h3>Poem ID: {match.params.id}</h3>
+    </div>
+  );
+}
+
+function Author({ match }) {
+  return (
+    <div>
+      <h3>Author ID: {match.params.id}</h3>
+    </div>
+  );
+}
 export default App;
