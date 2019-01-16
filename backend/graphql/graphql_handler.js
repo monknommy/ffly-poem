0d06e520f79d58614e23af802e6dc1ff782dcf69
Shawn Xu <xx9483@gmail.com>
2019-01-15 17:22:37 -0800

    no more if check in the code. make everything explicity config in config file.

--- backend/graphql/graphql_handler.js
+++ backend/graphql/graphql_handler.js
@@ -4,16 +4,11 @@ const { ApolloServer, gql } = require('apollo-server-lambda');
 const { schema } = require('./schema');
 const { resolvers } = require('./resolvers');
 
-const stage = process.env.STAGE;
-let endpoint = '/graphql';
-if (stage == 'dev') {
-  endpoint = '/dev/graphql';
-}
 const server = new ApolloServer({ 
   typeDefs: schema, 
   resolvers: resolvers,  
   playground: {
-    endpoint: endpoint,
+    endpoint: process.env.GRAPHQL_PLAYGROUND_ENDPOINT,
   }
 });
 
