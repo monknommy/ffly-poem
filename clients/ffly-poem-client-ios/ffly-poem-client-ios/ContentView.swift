d64ca782a05fc92f7ff86283c1871dee2fe08e0d
Shawn Xu <xx9483@gmail.com>
2021-08-06 09:58:40 -0700

    preparing IOS file

---
+++ clients/ffly-poem-client-ios/ffly-poem-client-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ffly-poem-client-ios
+//
+//  Created by Shawn Xu on 8/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ clients/ffly-poem-client-ios/ffly-poem-client-ios/ffly_poem_client_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ffly_poem_client_iosApp.swift
+//  ffly-poem-client-ios
+//
+//  Created by Shawn Xu on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ffly_poem_client_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
